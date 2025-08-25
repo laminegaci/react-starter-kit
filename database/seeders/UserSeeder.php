ad783add3cd87b1e22f225ac80b32ebf780a9e7a
mohamed lamine <mohamed61lamine@gmail.com>
2025-08-25 15:07:15 +0100

    `Updated UserSeeder to change email, password, and role of the team user, and reduced the number of users seeded from 100 to 50.`

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,10 +4,6 @@ namespace Database\Seeders;
 
 use Carbon\Carbon;
 use App\Models\User;
-use App\Enums\UserRole;
-use App\Models\Company;
-use App\Models\Profile;
-use App\Enums\UserGender;
 use App\Enums\UserRoleEnum;
 use Illuminate\Support\Str;
 use App\Enums\UserGenderEnum;
@@ -24,12 +20,12 @@ class UserSeeder extends Seeder
     public function run()
     {
         $team = User::create([
-            'email' => 'team@' . Str::lower(config('app.name', 'Laravel')) . '.com',
+            'email' => 'manager@' . Str::lower(config('app.name', 'Laravel')) . '.com',
             'password' => Hash::make('123456789'),
-        ])->assignRole(UserRoleEnum::USER->value);
+        ])->assignRole(UserRoleEnum::MANAGER->value);
 
         $team->profile()->create([
-            'first_name' => 'team',
+            'first_name' => 'manager',
             'last_name' => Str::lower(config('app.name', 'Laravel')),
             'full_name' => 'team ' . Str::lower(config('app.name', 'Laravel')),
             'phone_number' => '0699472366',
@@ -39,8 +35,7 @@ class UserSeeder extends Seeder
         ]);
 
 
-                // Seed 100 random users with profiles
-        User::factory(100)
-            ->create();
+        // Seed 100 random users with profiles
+        User::factory(50)->create();
     }
 }
